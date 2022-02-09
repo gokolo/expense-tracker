91ef6faa2ef4606ed4531fd12e36fc2b13f0dab2
gokolo <grace.okolo15@gmail.com>
2022-02-09 20:10:15 +0100

    try filtering by year

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
+
+import React, { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+  { id: 1, title: 'Car Insurance', amount: '$678.23', date: new Date(2021, 2, 28) },
+  { id: 2, title: 'Fire Insurance', amount: '$78.56', date: new Date(2021, 3, 28) },
+  { id: 3, title: 'Fire Alarm', amount: '$158.98', date: new Date(2021, 4, 28) },
+  { id: 4, title: 'Dinning Set', amount: '$2458.00', date: new Date(2021, 5, 28) },
+]
+
 function App() {
-  const expenses = [
-    { title: 'Car Insurance', amount: '$678.23', date: new Date(2021, 2, 28) },
-    { title: 'Fire Insurance', amount: '$78.56', date: new Date(2021, 3, 28) },
-    { title: 'Fire Alarm', amount: '$158.98', date: new Date(2021, 4, 28) },
-    { title: 'Dinning Set', amount: '$2458.00', date: new Date(2021, 5, 28) },
-  ]
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  let [filteredExpenses, setFilteredExpenses] = useState(expenses);
 
   const addExpenseHandler = (expense) => {
-    console.log('addExpenseHandler', expense)
+    setExpenses(prevExpenses => {
+      expense.date = new Date(expense.date);
+      expense.id = prevExpenses.length + 1;
+      setFilteredExpenses([expense, ...prevExpenses]);
+      return [expense, ...prevExpenses];
+    });
+
+  }
+
+  const filterExpensesHandler = (filterYear) => {
+    setFilteredExpenses(() => {
+      return expenses.filter(expense => {
+        console.log('filterExpensesHandler', filterYear, expense.date.getFullYear())
+        return expense.date.getFullYear().toString() === filterYear
+      })
+    });
   }
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses expenses={expenses} />
+      <Expenses expenses={filteredExpenses} onFilterExpenses={filterExpensesHandler} />
     </div>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -7,21 +7,21 @@ import './Expenses.css';
 
 
 function Expenses(props) {
-  const expenses = props.expenses;
   let [expenseFilter, setExpenseFilter] = useState('2020');
 
-  const filterExpensesHandler = (filterData) => {
-    setExpenseFilter(filterData)
+  const filterExpensesHandler = (filterYear) => {
+    setExpenseFilter(filterYear);
+    props.onFilterExpenses(filterYear);
   }
 
   return (
     <div>
       <Card className='expenses'>
         <ExpenseFilter selectedYear={expenseFilter} onFilterExpenses={filterExpensesHandler} />
-        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-        <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+
+        {props.expenses.map(expense =>
+          <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
+        )}
       </Card>
     </div>
   );
